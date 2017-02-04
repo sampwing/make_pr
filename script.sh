f28c86363c46167c17d5b93b3c860049eba9035f
Sam Wing <sampwing@gmail.com>
2017-02-03 16:07:11 -0800

    Stuff

--- script.sh
+++ script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 BRANCH_NAME=$(head -20 /dev/urandom | md5)
-git checkout -b $BRANCH_NAME
-git add .
+git checkout origin master
+git checkout -b $BRANCH_NAME -t origin/master
+git add script.sh
+git commit -c "Bump."
 git push origin $BRANCH_NAME
 echo "pushed branch $BRANCH_NAME"
-curl --user "$USERNAME:$PASSWORD" -XPOST https://api.github.com/repos/sampwing/make_pr/pulls --data "{\"title\": \"bump version\", \"head\": \"$BRANCH_NAME\", \"base\": \"master\"}"
+curl --header "Authorization: token $TOKEN" -XPOST https://api.github.com/repos/sampwing/make_pr/pulls --data "{\"title\": \"bump version\", \"head\": \"$BRANCH_NAME\", \"base\": \"master\"}" 
+# curl --header "Authorization: token $TOKEN" -XPOST https://api.github.com/repos/sampwing/make_pr/pulls --data "{\"title\": \"bump version\", \"base\": \"$BRANCH_NAME\", \"head\": \"master\"}" 
